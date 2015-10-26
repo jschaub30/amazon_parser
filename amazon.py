a0fa7c79ed3a84a2598b3e7134cb2ef02e79ab0a
Jeremy Schaub <schaubjeremy@gmail.com>
2015-10-26 09:14:19 -0500

    Added Mozilla header and changed to List Price

--- amazon.py
+++ amazon.py
@@ -4,7 +4,8 @@ from bs4 import BeautifulSoup
 
 # Keurig coffee machine
 url = 'http://www.amazon.com/gp/product/B004978NKY/'
-r = requests.get(url)
+url = 'http://www.amazon.com/Brita-Pitcher-Advanced-Replacement-Packaging/dp/B00004SU18/ref=zg_bs_kitchen_1'
+r = requests.get(url, headers = { 'User-Agent' : 'Mozilla/5.0' })
 
 #You could now simply parse r.text with somthing like this:
 #r.text.split('priceblock_ourprice')[1].split('>')[1].split('<')[0]
@@ -17,10 +18,14 @@ flag = False
 debug = True  # Print page if no price is found
 for td in td_list:
     if flag:
-        print td.span.text
+        try:
+            print td.span.text
+        except AttributeError:
+            print td.text
         flag = False
         debug = False
-    if 'Price:'==td.text:
+    #if 'Price:'==td.text:
+    if 'List Price:'==td.text:
         flag = True # Print the contents of the next cell too
         print td.text
 
